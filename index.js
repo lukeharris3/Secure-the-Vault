64feea10d5e79ce4883cb36874d5d027e0d4e2ca
lukeharris3 <166353470+lukeharris3@users.noreply.github.com>
2024-04-15 12:14:17 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+// create a string, assign three variables, add comments like this one, create a dialog box, submit assignment//
+// creating variables 10-40-39//
+const notification = "You have received this message because you have been chosen to open an important vault. Here is the secret combination"
+const number1 = 5 + 5; //answer= 5+5=10
+const number2= number1 * 4; //answer=10*4=40
+const number3= number2 - 1; //answer=40-1=39
+//final outoput for the combination
+alert(notification+" \nCombination 1: "+number1+"\nCombination 2: "+number2+"\nCombination 3: "+number3);
